16b2bf6ea7f6a7f5e638940d8c9a5331c65f62e9
ginokent <29125616+ginokent@users.noreply.github.com>
2024-08-30 00:50:02 +0900

    feat: Init

---
+++ internal/lang/go/generator/generate_each_file.go
@@ -0,0 +1,70 @@
+package generator
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/hakadoriya/z.go/errorz"
+	"github.com/hakadoriya/z.go/mustz"
+	"github.com/hakadoriya/z.go/otelz/tracez"
+
+	"github.com/hakadoriya/ormgen/internal/contexts"
+	"github.com/hakadoriya/ormgen/internal/lang/go/source"
+	"github.com/hakadoriya/ormgen/internal/logs"
+)
+
+func generateEachFile(ctx context.Context, ormoptPackageImportPath string, packageName string, packageSource *source.PackageSource, fileSource *source.FileSource, tablesInFile []*TableInfo) error {
+	ctx, span := tracez.Start(ctx)
+	defer span.End()
+
+	cfg := contexts.GenerateConfig(ctx)
+
+	// each file
+	eachFilePath := filepath.Join(cfg.GoORMOutputPath, fileSource.SourceRelativePath)
+	logs.Stdout.Debug("create file: file=" + eachFilePath)
+	var eachFile *os.File
+	if err := tracez.StartFuncWithSpanNameSuffix(ctx, "os.Create", func(_ context.Context) (err error) {
+		eachFile, err = os.Create(strings.TrimSuffix(eachFilePath, ".go") + ".orm.gen.go")
+		//nolint:wrapcheck
+		return err
+	}); err != nil {
+		return errorz.Errorf("os.Create: %w", err)
+	}
+	defer eachFile.Close()
+
+	_ = tracez.StartFuncWithSpanNameSuffix(ctx, "template.New", func(_ context.Context) (err error) {
+		eachFileTemplateOnce.Do(func() {
+			eachFileTemplate = template.Must(template.New(eachFileTmpl).Funcs(templateFuncMap(cfg)).Parse(string(mustz.One(templates.ReadFile(eachFileTmpl)))))
+		})
+		return nil
+	})
+
+	eachFileBuf := bytes.NewBuffer(nil)
+
+	if err := tracez.StartFuncWithSpanNameSuffix(ctx, "eachFileTemplate.Execute", func(_ context.Context) (err error) {
+		return eachFileTemplate.Execute(eachFileBuf, FileInfo{
+			SourceFile:              fileSource.SourceRelativePath,
+			PackageName:             packageName,
+			PackageImportPath:       packageSource.PackageImportPath,
+			ORMOptPackageImportPath: ormoptPackageImportPath,
+			Dialect:                 cfg.Dialect,
+			SliceTypeSuffix:         cfg.GoSliceTypeSuffix,
+			Tables:                  tablesInFile,
+		})
+	}); err != nil {
+		return errorz.Errorf("eachFileTemplate.Execute: %w", err)
+	}
+
+	if err := tracez.StartFuncWithSpanNameSuffix(ctx, "eachFile.Write", func(_ context.Context) (err error) {
+		_, err = eachFile.Write(eachFileBuf.Bytes())
+		return err //nolint:wrapcheck
+	}); err != nil {
+		return errorz.Errorf("eachFile.Write: %w", err)
+	}
+
+	return nil
+}
---
+++ internal/lang/go/source/work_dir_test.go
@@ -0,0 +1,90 @@
+package source
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"io/fs"
+	"log/slog"
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/hakadoriya/z.go/buildz"
+	"github.com/hakadoriya/z.go/testingz/assertz"
+	"github.com/hakadoriya/z.go/testingz/requirez"
+
+	"github.com/hakadoriya/ormgen/internal/config"
+	"github.com/hakadoriya/ormgen/internal/contexts"
+	"github.com/hakadoriya/ormgen/internal/logs"
+)
+
+func Test_walkDirFn(t *testing.T) {
+	t.Parallel()
+
+	t.Run("error,errArg", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := contexts.WithGenerateConfig(context.Background(), &config.GenerateConfig{
+			GoColumnTag: goColumnTag,
+		})
+		err := newParseWalkDir(ctx, "", ".dat", nil)("", nil, io.EOF)
+		requirez.ErrorIs(t, err, io.EOF)
+	})
+
+	t.Run("error,d.IsDir", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := contexts.WithGenerateConfig(context.Background(), &config.GenerateConfig{
+			GoColumnTag: goColumnTag,
+		})
+		info, errStat := os.Stat(validSourcePath)
+		requirez.NoError(t, errStat)
+		err := newParseWalkDir(ctx, "", ".dat", nil)("", fs.FileInfoToDirEntry(info), nil)
+		requirez.NoError(t, err)
+	})
+
+	t.Run("error,no-such-file-or-directory.dat", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := contexts.WithGenerateConfig(context.Background(), &config.GenerateConfig{
+			GoColumnTag: goColumnTag,
+		})
+		trace := bytes.NewBuffer(nil)
+		ctx = contexts.WithTrace(ctx, logs.NewTrace(trace, slog.LevelDebug))
+		info, errStat := os.Stat(validSourceFileUser)
+		requirez.NoError(t, errStat)
+		err := newParseWalkDir(ctx, "", ".dat", nil)("no-such-file-or-directory.dat", fs.FileInfoToDirEntry(info), nil)
+		requirez.NoError(t, err)
+		actualLog := trace.String()
+		assertz.StringMatchRegex(t, actualLog, regexp.MustCompile(`"severity":"DEBUG","caller":"source/work_dir.go:\d+","msg":"skip: path=no-such-file-or-directory.dat: parser.ParseFile=open no-such-file-or-directory.dat: no such file or directory: no source found","logName":"trace","app":"ormgen"}`))
+	})
+
+	t.Run("error,buildz.ErrReachedRootDirectory", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := contexts.WithGenerateConfig(context.Background(), &config.GenerateConfig{
+			GoColumnTag: goColumnTag,
+		})
+		trace := bytes.NewBuffer(nil)
+		ctx = contexts.WithTrace(ctx, logs.NewTrace(trace, slog.LevelDebug))
+		info, errStat := os.Stat(validSourceFileUser)
+		requirez.NoError(t, errStat)
+		err := newParseWalkDir(ctx, "/", fileExt, nil)(validSourceFileUser, fs.FileInfoToDirEntry(info), nil)
+		requirez.ErrorIs(t, err, buildz.ErrReachedRootDirectory)
+	})
+
+	t.Run("error,filepath.Rel", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := contexts.WithGenerateConfig(context.Background(), &config.GenerateConfig{
+			GoColumnTag: goColumnTag,
+		})
+		trace := bytes.NewBuffer(nil)
+		ctx = contexts.WithTrace(ctx, logs.NewTrace(trace, slog.LevelDebug))
+		info, errStat := os.Stat(validSourceFileUser)
+		requirez.NoError(t, errStat)
+		err := newParseWalkDir(ctx, "..", fileExt, nil)(validSourceFileUser, fs.FileInfoToDirEntry(info), nil)
+		requirez.ErrorContains(t, err, `Rel: can't make testdata/valid/user/user.go relative to ..`)
+	})
+}
